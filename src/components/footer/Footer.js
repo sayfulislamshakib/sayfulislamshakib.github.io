f2ca58dee6cbd16a6b7e1cfe04516bbdd1764aea
Sayful Islam Shakib <76785210+sayfulislamshakib@users.noreply.github.com>
2021-05-11 10:18:24 +0600

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Copyright {greeting.title2}
+          © {greeting.title2}
         </p>
       </Fade>
     </div>
